1d318b16b3291d360f87ac967ed17890d7ae8b35
Omkar <omkarsalunke1978@gmail.com>
2024-03-23 10:18:14 +0530

    updated files (part 1)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import Add from "./components/Add";
+import Greetings from "./components/Greetings";
 
-function App() {
+import { Fragment } from "react"; // no need of this while using <> </>
+
+function Topic1() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello my name is Omkar</h1>
+      {/* <p class="initial-class">Welcome to the react world!</p> */}
+      {/* The above sentence is WRONG; class in JS is 
+      a totally different thing; so we have to use 'className' */}
+      <p className="init-class">Welcome to the React world</p>
     </div>
   );
 }
 
-export default App;
+function Topic2() {
+  return <p> Hope I master it</p>;
+}
+
+function Topic3() {
+  return (
+    <form>
+      <label htmlFor="name">Name</label>
+      <input type="text" id="name" />
+    </form>
+  );
+}
+
+function Topic4() {
+  return <Add />;
+}
+
+function Topic5() {
+  return (
+    <Fragment>
+      <Greetings />
+      <Add />
+      <p>Roger, Confirming that the code is in Fragment</p>
+    </Fragment>
+  );
+}
+
+function Topic6() {
+  return (
+    <>
+      <p>
+        This is a shorter way to use Fragments! you can also remove the above
+        import statement!
+      </p>
+    </>
+  );
+}
+
+function Topic7() {
+  const name = "PROmkar";
+  const mul = (a, b) => a * b;
+  const demoClass = "simple-class";
+
+  return (
+    <>
+      <h1>Hello gaeys orewa {name} desu.</h1>
+      <p>7 multiplied by 6 is {mul(7, 6)}</p>
+      <p>
+        My friends list is:{" "}
+        {["Alex ", "John ", "Ryan Gosling ", "Aana De Armas"]}
+      </p>
+
+      <p className={demoClass}>I have given this para a simple-class!</p>
+      <p>Hello.. Hello, Is it working properly over??</p>
+      <p>Yes it is! Over n out!</p>
+    </>
+  );
+}
+export default Topic7;
