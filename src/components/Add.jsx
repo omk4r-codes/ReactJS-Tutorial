8f96f4274169379e75f8f80c59157b9c6d8c5951
Omkar <omkarsalunke1978@gmail.com>
2024-03-23 10:19:33 +0530

    updated files (part 1)

---
+++ src/components/Add.jsx
@@ -0,0 +1,6 @@
+// btw, you can give components file extension as .js or .jsx!
+
+const Add = () => {
+  return <div>Hi I'm from add component!</div>;
+};
+export default Add;
---
+++ src/components/Greetings.jsx
@@ -0,0 +1,5 @@
+const Greetings = () => {
+  return <div>Final regards master, I'm from the Greetings component</div>;
+};
+
+export default Greetings;
